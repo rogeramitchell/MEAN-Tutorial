016c3283763e57bb34843553542208a3fe9a8873
Roger Mitchell <rogeramitchell@gmail.com>
2015-09-26 21:20:16 -0400

    require comments from app.js

--- MEAN/app.js
+++ MEAN/app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
 
 var app = express();
 
