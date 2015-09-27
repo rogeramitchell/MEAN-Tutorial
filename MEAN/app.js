5cd590e3bee660060efeafad302c500ff4c1040c
Roger Mitchell <rogeramitchell@gmail.com>
2015-09-26 21:04:14 -0400

    required changes for mongo in app.js

--- MEAN/app.js
+++ MEAN/app.js
@@ -8,7 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
