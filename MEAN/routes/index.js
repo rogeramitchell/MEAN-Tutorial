cb15a601aec510b8c6ddd2477e45f8dc2c1f42c9
Roger Mitchell <rogeramitchell@gmail.com>
2015-09-27 10:49:44 -0400

    added route for inserting new comment

--- MEAN/routes/index.js
+++ MEAN/routes/index.js
@@ -51,4 +51,20 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	});
 });
 
+router.post('/posts/:post/comments', function(req, res, next) {
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save(function(err, comment) {
+		if(err) { return next(err); }
+
+		req.post.comments.push(comment);
+		req.post.save(function(err, post) {
+			if(err) { return next(err); }
+
+			res.json(comment);
+		});
+	});
+});
+
 module.exports = router;
