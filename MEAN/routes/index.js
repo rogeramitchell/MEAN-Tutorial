f143563dc8054c88292ae8a9544c58d62207ee1d
Roger Mitchell <rogeramitchell@gmail.com>
2015-09-27 11:00:54 -0400

    added param for comment, route for calling comment upvote method

--- MEAN/routes/index.js
+++ MEAN/routes/index.js
@@ -39,16 +39,20 @@ router.param('post', function(req, res, next, id) {
 	});
 });
 
-router.get('/posts/:post', function(req, res) {
-	res.json(req.post);
+router.param('comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function(err, comment) {
+		if(err) { return next(err); }
+		if(!comment) { return next(new Error('can\'t find comment')); }
+
+		req.comment = comment;
+		return next();
+	});
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
-	req.post.upvote(function(err, post) {
-		if(err) { return next(err); }
-
-		res.json(post);
-	});
+router.get('/posts/:post', function(req, res) {
+	res.json(req.post);
 });
 
 router.post('/posts/:post/comments', function(req, res, next) {
@@ -67,4 +71,36 @@ router.post('/posts/:post/comments', function(req, res, next) {
 	});
 });
 
+router.put('/posts/:post/upvote', function(req, res, next) {
+	req.post.upvote(function(err, post) {
+		if(err) { return next(err); }
+
+		res.json(post);
+	});
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+	req.comment.upvote(function(err, post) {
+		if(err) { return next(err) };
+
+		res.json(comment);
+	});
+});
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
